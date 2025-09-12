98b24ae07400cb8141159ac3a86b43337490b46c
RifabAhamed <rifabahamed@gmail.com>
2025-09-12 21:15:34 +0530

    job apis completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import dbConfig from "./configs/dbConfig.js";
 import userRoutes from "./routes/UserRoutes.js";
 import companyRoutes from "./routes/companyRoutes.js";
+import jobRoutes from "./routes/JobRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/v1/platform/user", userRoutes);
 app.use("/v1/platform/company", companyRoutes);
+app.use("/v1/platform/job", jobRoutes);
 
 
 async function startServer(){
